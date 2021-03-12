11272ee38b5f51cbd94a487784f9218436c3c4b8
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-03-12 16:11:10 -0500

    creando el archivo de rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PaisModule, SharedModule],
+  imports: [BrowserModule, AppRoutingModule, PaisModule, SharedModule],
   providers: [],
   bootstrap: [AppComponent],
 })
