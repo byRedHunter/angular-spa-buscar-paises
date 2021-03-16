e1157885895dd77022163d9cd987ae6719a75840
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-03-16 17:04:29 -0500

    mostrar paises por region

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
@@ -12,14 +14,27 @@ import { Component, OnInit } from '@angular/core';
   ],
 })
 export class PorRegionComponent implements OnInit {
-  regiones: string[] = ['africa', 'americas', 'oceania', 'europa', 'asia'];
+  regiones: string[] = ['africa', 'americas', 'oceania', 'europe', 'asia'];
   regionActiva: string = '';
+  paises: Country[] = [];
+  loading: boolean = false;
 
-  constructor() {}
+  constructor(private paisService: PaisService) {}
 
   ngOnInit(): void {}
 
   activarRegion(region: string) {
+    if (region === this.regionActiva) return;
+
+    this.loading = true;
     this.regionActiva = region;
+    this.paises = [];
+
+    this.paisService
+      .buscarPaisesPorRegion(this.regionActiva)
+      .subscribe((paises) => {
+        this.paises = paises;
+        this.loading = false;
+      });
   }
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -28,4 +28,10 @@ export class PaisService {
 
     return this.http.get<Country>(url);
   }
+
+  buscarPaisesPorRegion(region: string): Observable<Country[]> {
+    const url: string = `${this.url}/region/${region}`;
+
+    return this.http.get<Country[]>(url);
+  }
 }
