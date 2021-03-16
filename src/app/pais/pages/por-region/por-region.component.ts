3608c51d33c9bab47d18a36c158012099d428abc
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-03-16 11:59:22 -0500

    mostrar clases dinamicamente

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -4,13 +4,22 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-por-region',
   templateUrl: './por-region.component.html',
   styles: [
-  ]
+    `
+      button {
+        margin: 0 10px 10px 0;
+      }
+    `,
+  ],
 })
 export class PorRegionComponent implements OnInit {
+  regiones: string[] = ['africa', 'americas', 'oceania', 'europa', 'asia'];
+  regionActiva: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  activarRegion(region: string) {
+    this.regionActiva = region;
   }
-
 }
